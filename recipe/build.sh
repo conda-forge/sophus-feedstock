e58316380febaa0a37570f795a4ef22590c5f632
Jeongseok Lee <jeongseok@meta.com>
2024-06-22 08:20:49 -0700

    Add win64 build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,22 @@
-mkdir -p build
-cd build
-cmake ${CMAKE_ARGS} \
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ "${target_platform}" == osx-* ]]; then
+  # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+cmake $SRC_DIR \
+      ${CMAKE_ARGS} \
+      -G Ninja \
+      -B build \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
-      -DBUILD_SOPHUS_TESTS=ON \
-      ..
-make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  ctest --output-on-failure
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SOPHUS_TESTS=ON
+cmake --build build --parallel
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  ctest --test-dir build --output-on-failure
 fi
-make install
+cmake --build build --target install
